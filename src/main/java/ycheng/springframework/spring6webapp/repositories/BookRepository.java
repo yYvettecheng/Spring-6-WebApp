27042a460e39085b826917fb0f4a77c10e813df8
Yiwen Cheng <48226157+nikkicheng1125@users.noreply.github.com>
2025-02-19 23:01:29 -0800

    Created JPA repositories.

---
+++ src/main/java/ycheng/springframework/spring6webapp/repositories/BookRepository.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Yiwen Cheng on 2/19/25
+ */
+
+package ycheng.springframework.spring6webapp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import ycheng.springframework.spring6webapp.domain.Book;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+
+}
